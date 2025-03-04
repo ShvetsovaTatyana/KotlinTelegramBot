3efef4ab48496dd6143e76bdc601df0eaa2e8e93
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2025-03-04 19:13:22 +0300

    KTB-15 fixed

--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -6,19 +6,22 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
-class TelegramBotService {
-    fun getUpdates(botToken: String, updateId: Int): String {
-        val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
-        val client: HttpClient = HttpClient.newBuilder().build()
+const val API_ADDRESS = "https://api.telegram.org/bot"
+
+class TelegramBotService(var client: HttpClient, var botToken: String) {
+
+    fun getUpdates(updateId: Int): String {
+        val urlGetUpdates = "$API_ADDRESS$botToken/getUpdates?offset=$updateId"
+        client = HttpClient.newBuilder().build()
         val requestUpdate: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
         val responseUpdate: HttpResponse<String> = client.send(requestUpdate, HttpResponse.BodyHandlers.ofString())
         return responseUpdate.body()
     }
 
-    fun sendMessage(botToken: String, chatId: String, text: String) {
+    fun sendMessage(chatId: String, text: String) {
         val urlSendMessage =
-            "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=${URLEncoder.encode(text, "UTF-8")}"
-        val client: HttpClient = HttpClient.newBuilder().build()
+            "$API_ADDRESS$botToken/sendMessage?chat_id=$chatId&text=${URLEncoder.encode(text, "UTF-8")}"
+        client = HttpClient.newBuilder().build()
         val requestSendMessage: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
         client.send(requestSendMessage, HttpResponse.BodyHandlers.ofString())
     }
